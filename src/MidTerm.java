506eb7d42e81f100ca777315bf8e338c47413ba7
David Wolverton <dwolvert@gmail.com>
2020-05-07 11:03:56 -0400

    once again

--- src/MidTerm.java
+++ src/MidTerm.java
@@ -2,7 +2,7 @@
 public class MidTerm {
 
 	public static void main(String[] args) {
-		System.out.println("Twice upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
+		System.out.println("Once upon a time in the mystical land of Shangrila there dwelt an immortal named Kai-Yun and his trusty band of ninja monkeys.");
 		System.out.println("The ninja monkeys craved yogurt covered bananas!");
 	}
 
